df9826b640bd7e726415a612237724799dabe719
tan-xianglong <t.xianglong@gmail.com>
2022-02-13 02:07:01 +0800

    added portfolio section, scroll snap and change of logo color after certain height

--- js/script.js
+++ js/script.js
@@ -11,12 +11,41 @@ const navToggle = () => {
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollpos = window.pageYOffset;
+const logo1A = document.querySelectorAll(".image-container .nav-logo1")[0];
+const logo2A = document.querySelectorAll(".image-container .nav-logo2")[0];
+const logo1B = document.querySelectorAll(".image-container .nav-logo1")[1];
+const logo2B = document.querySelectorAll(".image-container .nav-logo2")[1];
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".navbar-desktop").style.top = "0";
   } else {
     document.querySelector(".navbar-desktop").style.top = "-54px";
   }
   prevScrollpos = currentScrollPos;
+  let viewPortHeight = window.innerHeight;
+  if (document.scrollingElement.scrollTop >= viewPortHeight) {
+    logo1A.style.display = "none";
+    logo2A.style.display = "block";
+    logo1B.style.display = "none";
+    logo2B.style.display = "block";
+  } else {
+    logo1A.style.display = "block";
+    logo2A.style.display = "none";
+    logo1B.style.display = "block";
+    logo2B.style.display = "none";
+  }
 }
+
+// Smooth scrolling to targeted section
+document.querySelectorAll('a[href^="#portfolio"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+//change logo color according to scroll offset
