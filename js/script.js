86d10ea6237bfc9f4b0cb7b8aa9a6f1794e08c2f
tan-xianglong <t.xianglong@gmail.com>
2022-02-14 00:35:44 +0800

    added ability for sections to appear when scroll into view

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@ const navToggle = () => {
   }
 };
 
+//helper function to detect if element is in portview
+const isInViewport = (elem) => {
+  let bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 
 //change navbar logo and text color according to scroll offset
@@ -22,6 +35,7 @@ const logo1B = document.querySelectorAll(".image-container .nav-logo1")[1];
 const logo2B = document.querySelectorAll(".image-container .nav-logo2")[1];
 const hamburger = document.querySelector(".fa-bars");
 const desktopNav = document.querySelectorAll(".link-desktop");
+const fadein = document.querySelectorAll(".fadein");
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -46,4 +60,10 @@ window.onscroll = function() {
     hamburger.style.color = "#fff";
     desktopNav.forEach((elem) => (elem.style.color = "#fff"));
   }
+  fadein.forEach(elem => {
+    if(isInViewport(elem)){
+      elem.style.visibility = "visible";
+      elem.style.opacity = "1";
+    }
+  })
 }
